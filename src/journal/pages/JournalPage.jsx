60478278f99e63182176774547053a010990808c
Victor <vrmm1998@gmail.com>
2024-02-25 13:12:53 -0500

    add view for when the user creates a note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,49 @@
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { ImageGallery } from '../components';
+
+export const NoteView = () => {
+  return (
+    <Grid
+      container
+      direction='row'
+      justifyContent='space-between'
+      alignItems='center'
+      sx={{ mb: 1 }}
+    >
+      <Grid item>
+        <Typography fontSize={24} fontWeight='light'>
+          25 de Febrero de 2024
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button color='inherit' sx={{ p: 1 }}>
+          <SaveOutlined />
+          <Typography sx={{ fontSize: 16, ml: 1 }}>Guardar</Typography>
+        </Button>
+      </Grid>
+
+      <Grid container>
+        <TextField
+          type='text'
+          variant='filled'
+          placeholder='Título de tu nota'
+          label='Titulo'
+          sx={{ border: 'none', my: 1 }}
+          fullWidth
+        />
+        <TextField
+          type='text'
+          variant='filled'
+          multiline
+          minRows={5}
+          placeholder='¿Que deseas recordar hoy?'
+          fullWidth
+        />
+      </Grid>
+
+      {/* Galeria de imagenes */}
+      <ImageGallery />
+    </Grid>
+  );
+};
