f68433015cc2c7aa517f64fb8f0be5dce3e41fdf
Victor <vrmm1998@gmail.com>
2024-02-27 12:22:48 -0500

    add redux store to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './router/AppRouter.jsx';
 import './styles.css';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </React.StrictMode>
 );
