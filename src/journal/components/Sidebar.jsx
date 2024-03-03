940816fd1817fb88aebb68ad316f8bec46ae4519
Victor <vrmm1998@gmail.com>
2024-03-03 16:23:04 -0500

    load notes to sidebar from store

--- src/journal/components/Sidebar.jsx
+++ src/journal/components/Sidebar.jsx
@@ -1,58 +1,33 @@
-import { useSelector } from "react-redux";
-import { TurnedInNot } from "@mui/icons-material";
-import {
-  Box,
-  Divider,
-  Drawer,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { useSelector } from 'react-redux';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
+import { SidebarItem } from './';
 
 export const Sidebar = ({ drawerWidth = 240 }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
 
   return (
     <Box
-      component="nav"
+      component='nav'
       sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
     >
       <Drawer
-        variant="permanent"
+        variant='permanent'
         open
         sx={{
-          display: { xs: "block" },
-          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
         }}
       >
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant='h6' noWrap component='div'>
             {displayName}
           </Typography>
         </Toolbar>
         <Divider />
         <List>
-          {["Enero", "Febrero", "Marzo", "Abril", "Mayo"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      "Este es un texto de prueba creado porque no tengo lorem ipsum"
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SidebarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
--- src/journal/components/index.js
+++ src/journal/components/index.js
@@ -1,3 +1,4 @@
-export * from "./ImageGallery";
-export * from "./Navbar";
-export * from "./Sidebar";
+export * from './ImageGallery';
+export * from './Navbar';
+export * from './Sidebar';
+export * from './SidebarItem';
